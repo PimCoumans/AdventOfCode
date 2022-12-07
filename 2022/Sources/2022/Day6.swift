bdf55458ab9576e54b101c1d1b3cfd23fe769828
Pim <pim@pixelrock.nl>
2022-12-07 15:55:17 +0100

    ⭐️ Day 6

--- 2022/Sources/2022/Day6.swift
+++ 2022/Sources/2022/Day6.swift
@@ -1,18 +1,40 @@
 import Foundation
 import Algorithms
 
+extension Collection where Element: Hashable {
+	var isUnique: Bool {
+		Set(self).count == count
+	}
+}
+
 struct Day6: Day {
 
 	let input: String
 	init(input: String) {
 		self.input = input
 	}
+
+	func indexAfterUniqueSequence(ofLength length: Int, in string: String) -> Int? {
+		var markerSequence: [Character] = []
+		return string
+			.enumerated()
+			.first { index, character in
+				markerSequence.append(character)
+				guard markerSequence.count > length else {
+					return false
+				}
+				markerSequence.removeFirst()
+				return markerSequence.isUnique
+			}
+			.map(\.offset)
+			.map { $0 + 1 }
+	}
 	
 	func partOne() -> String {
-		""
+		String(indexAfterUniqueSequence(ofLength: 4, in: input)!)
 	}
 	
 	func partTwo() -> String {
-		""
+		String(indexAfterUniqueSequence(ofLength: 14, in: input)!)
 	}
 }
