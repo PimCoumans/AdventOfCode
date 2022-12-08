cba6db239f1ee0b8f28c81e2b4fc519e17aac11b
Pim <pim@pixelrock.nl>
2022-12-08 09:04:04 +0100

    ⭐️ Day 7

--- 2022/Sources/2022/Day7.swift
+++ 2022/Sources/2022/Day7.swift
@@ -2,16 +2,76 @@ import Foundation
 
 struct Day7: Day {
 
-	let input: String
+	enum Line {
+		case goUp
+		case goto(directory: String)
+		case list // Unused
+
+		case file(size: Int)
+		case directory(name: String) // Unused
+
+		init?(string: String) {
+			let parts = string.components(separatedBy: .whitespaces)
+
+			if parts.first == "$" {
+				let command = parts[1]
+				let parameter = parts.count > 2 ? parts[2] : nil
+				switch (command, parameter) {
+				case ("ls", .none): self = .list
+				case ("cd", ".."): self = .goUp
+				case ("cd", .some(let string)): self = .goto(directory: string)
+				default: return nil
+				}
+			} else {
+				let (description, name) = parts.firstTwoValues
+				switch (description, name) {
+				case ("dir", let name): self = .directory(name: name)
+				case (let size, _) where Int(size) != nil: self = .file(size: Int(size)!)
+				default: return nil
+				}
+			}
+		}
+	}
+
+	let directorySizes: [Int]
+
 	init(input: String) {
-		self.input = input
+
+		var currentPath: [Int] = []
+		var sizes: [Int] = []
+
+		for line in input.components(separatedBy: .newlines) {
+			switch Line(string: line) {
+			case .goUp: // Leaving dir, add size to sizes
+				sizes.append(currentPath.removeLast())
+			case .goto(directory: _): // Entering dir, add new size to path
+				currentPath.append(0)
+			case .file(size: let fileSize): // Update all sizes in current path
+				currentPath = currentPath.map { $0 + fileSize }
+			default: continue
+			}
+		}
+
+		sizes.append(contentsOf: currentPath)
+		self.directorySizes = sizes
 	}
 
 	func partOne() -> String {
-		""
+		let maxSize = 100_000
+		let totalSize = directorySizes
+			.filter { $0 <= maxSize }
+			.reduce(0, +)
+		return String(totalSize)
 	}
 
 	func partTwo() -> String {
-		""
+		let totalDiskSpace = 700_00_000
+		let diskSpaceRequired = 300_00_000
+		let diskSpaceFree = totalDiskSpace - directorySizes.max()!
+		let spaceToDelete = diskSpaceRequired - diskSpaceFree
+		let directoryToDelete = directorySizes
+			.filter { $0 >= spaceToDelete }
+			.min()!
+		return String(directoryToDelete)
 	}
 }
