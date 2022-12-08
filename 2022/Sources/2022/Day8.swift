f717a44a0bc0831bcee9b184dde49fb83bf4617b
Pim <pim@pixelrock.nl>
2022-12-08 09:24:44 +0100

    Setup day 8

---
+++ 2022/Sources/2022/Day8.swift
@@ -0,0 +1,16 @@
+import Foundation
+import Algorithms
+
+struct Day8: Day {
+
+	init(input: String) {
+	}
+
+	func partOne() -> String {
+		return ""
+	}
+
+	func partTwo() -> String {
+		return ""
+	}
+}
---
+++ 2022/Sources/2022/Inputs/Day8Input.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+extension Day8 {
+	static var input: String { """
+<-- PASTE DAY 1 PUZZLE INPUT FROM ADVENTOFCODE.COM HERE -->
+"""
+	}
+}
