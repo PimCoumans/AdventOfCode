9a6c39e8359b933b5f41f32cadade0a328fea9d2
Pim <pim@pixelrock.nl>
2022-12-13 23:41:44 +0100

    Prepare day 13

---
+++ 2022/Sources/2022/Day13.swift
@@ -0,0 +1,19 @@
+import Foundation
+import Algorithms
+
+struct Day13: Day {
+
+	let input: String
+
+	init(input: String) {
+		self.input = input
+	}
+
+	func partOne() -> Int {
+		0
+	}
+
+	func partTwo() -> Int {
+		0
+	}
+}
---
+++ 2022/Sources/2022/Inputs/Day13Input.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+extension Day13 {
+	static var input: String { """
+<-- PASTE DAY 13 PUZZLE INPUT FROM ADVENTOFCODE.COM HERE -->
+"""
+	}
+}
