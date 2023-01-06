a17e2184a06b9e0547ee893e5a25de65cc428394
Pim <pim@pixelrock.nl>
2023-01-06 12:10:13 +0100

    Keeping track of execution duration for each day

--- 2022/Sources/2022/main.swift
+++ 2022/Sources/2022/main.swift
@@ -18,7 +18,10 @@ var days: [any Day] = [
 ]
 
 for (dayNumber, day) in days.enumerated() {
+	let time = DispatchTime.now()
 	print("⭐️ Day \(dayNumber + 1)")
 	print("- Part one: \(day.partOne())")
 	print("- Part two: \(day.partTwo())")
+	let difference = DispatchTime.now().uptimeNanoseconds - time.uptimeNanoseconds
+	print("- Duration: \(Double(difference) / 1_000_000_000)")
 }
