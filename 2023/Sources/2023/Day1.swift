13f21dd06f2c863614301b225d393e88b5dde059
Pim <pim@pixelrock.nl>
2023-12-01 10:39:22 +0100

    ⭐️⭐️ Day 1

--- 2023/Sources/2023/Day1.swift
+++ 2023/Sources/2023/Day1.swift
@@ -1,23 +1,60 @@
 import Foundation
 
 struct Day1: Day {
+	let numbers: [String: Int]
+
 	let input: String
 	init(input: String) {
 		self.input = input
+//		self.input = """
+//two1nine
+//eightwothree
+//abcone2threexyz
+//xtwone3four
+//4nineeightseven2
+//zoneight234
+//7pqrstsixteen
+//"""
+		var numbers = [String: Int]()
+		let numberFormatter = NumberFormatter()
+		numberFormatter.numberStyle = .spellOut
+		for int in 0...9 {
+			numbers["\(int)"] = int
+			numbers[numberFormatter.string(from: NSNumber(value: int))!] = int
+		}
+		self.numbers = numbers
 	}
 
 	func partOne() -> Int {
-		let numbers = input
+		input
 			.lines()
 			.map {
 				String($0.first(where: { $0.isNumber })!) +
 				String($0.last(where: { $0.isNumber })!)
 			}
-			.map { Int($0)! }
-		return numbers.sum()
+			.compactMap(Int.init)
+			.sum()
+	}
+
+	func findInteger(in string: String, options: String.CompareOptions = []) -> Int? {
+		let ranges = numbers.compactMap { string.range(of: $0.key, options: options) }
+		let foundRange: Range<String.Index>! = options == .backwards ? ranges.max(\.upperBound) : ranges.min(\.lowerBound)
+		return numbers[String(string[foundRange])]
+	}
+
+	func findCombinedInteger(in string: String) -> Int? {
+		guard
+			let firstInt = findInteger(in: string),
+			let lastInt = findInteger(in: string, options: .backwards) else {
+			return nil
+		}
+		return firstInt * 10 + lastInt
 	}
 
 	func partTwo() -> Int {
-		0
+		input
+			.lines()
+			.compactMap(findCombinedInteger(in:))
+			.sum()
 	}
 }
