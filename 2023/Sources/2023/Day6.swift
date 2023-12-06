6fd28c29ba1e6c684d382a11a9b440b4d67ac2a3
Pim <pim@pixelrock.nl>
2023-12-06 10:43:14 +0100

    Day 6 ⭐️

--- 2023/Sources/2023/Day6.swift
+++ 2023/Sources/2023/Day6.swift
@@ -2,15 +2,56 @@ import Foundation
 
 struct Day6: Day {
 
+	let times: [Int]
+	let scores: [Int]
+
 	let input: String
 	init(input: String) {
-//		self.input = input
-		self.input = """
-"""
+		self.input = input
+//		self.input = """
+//Time:      7  15   30
+//Distance:  9  40  200
+//"""
+		let (times, distances) = self.input
+			.linesByDroppingTrailingEmpty()
+			.map {
+				$0
+					.components(separatedBy: ":")
+					.last!
+					.components(separatedBy: .whitespaces)
+					.compactMap(Int.init)
+			}
+			.firstTwoValues
+
+		self.times = times
+		self.scores = distances
+	}
+
+	func distance(for time: Int, speed: Int) -> Int {
+		return time * speed
+	}
+
+	func ranges(for time: Int, toBeat score: Int) -> Int {
+		var result: (min: Int, max: Int) = (.max, 0)
+		for holdTime in 1..<time {
+			let distance = distance(for: time - holdTime, speed: holdTime)
+			guard distance > score else {
+				continue
+			}
+			if holdTime < result.min {
+				result.min = holdTime
+			}
+			if holdTime > result.max {
+				result.max = holdTime
+			}
+		}
+		return (result.max - result.min) + 1
 	}
 
 	func partOne() -> Int {
-		0
+		zip(times, scores)
+			.map { ranges(for: $0, toBeat: $1) }
+			.reduce(1, *)
 	}
 
 	func partTwo() -> Int {
