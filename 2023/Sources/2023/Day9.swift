c48af562d6a30ac75a62e7e98e3193611ed00eec
Pim <pim@pixelrock.nl>
2023-12-15 10:21:00 +0100

    Day 9 ⭐️

--- 2023/Sources/2023/Day9.swift
+++ 2023/Sources/2023/Day9.swift
@@ -1,15 +1,51 @@
 import Foundation
 
 struct Day9: Day {
+
+	let values: [[Int]]
+
 	let input: String
 	init(input: String) {
-//		self.input = input
-		self.input = """
-"""
+		self.input = input
+//		self.input = """
+//-3 0 3 6 9 12 15
+//1 3 6 10 15 21
+//10 13 16 21 30 45
+//"""
+		self.values = self.input
+			.linesByDroppingTrailingEmpty()
+			.map { $0
+				.components(separatedBy: .whitespaces)
+				.compactMap(Int.init)
+			}
 	}
 
-	func partOne() -> Int {
-		0
+	func predictedValue(for history: [Int], debug: Bool = false) -> Int {
+		var differences: [[Int]] = [history]
+		while differences.last!.sum() != 0 {
+			differences.append(
+				Array(
+					differences
+						.last!
+						.windows(ofCount: 2)
+						.map(\.firstTwoValues)
+						.map { $1 - $0 }
+				)
+			)
+		}
+		if debug {
+			for row in differences {
+				print(row)
+			}
+		}
+		return differences
+			.map { $0.last! }
+			.sum()
+	}
+
+	func partOne() -> Int {return values.map {
+			predictedValue(for: $0)
+		}.sum()
 	}
 
 	func partTwo() -> Int {
